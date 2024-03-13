e20bce6d2e29f5bc5c9d883a2f72a4b093d145ee
Rayane Aberkane <nyrozzhd.gaming@gmail.com>
2024-03-13 03:09:28 +0100

    minimal working version

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-console.log("Hello node!");
+
+const express = require('express');
+const { readFile } = require('fs');
+
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    
+    readFile('./home.html', 'utf8', (err, html) => {
+        if (err) {
+            res.status(500).send("Sorry, Error");
+        }       
+        res.send(html);
+    })
+
+
+}); 
+
+
+app.listen(process.env.PORT || 3000, () => console.log('listening on localhost port 3000 or (rayanegpt.com)'));
